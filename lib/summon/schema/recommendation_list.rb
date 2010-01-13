956c8844aa6e9005b1fbae9f968f694f357db25d
Mando Escamilla <mando@mando.org>
2010-01-13 03:31:08 -0600

    restructured the recommendationlist due to structure oversight on my part.

--- lib/summon/schema/recommendation_list.rb
+++ lib/summon/schema/recommendation_list.rb
@@ -1,5 +1,15 @@
 class Summon::RecommendationList < Summon::Schema
   attr :database
-  attr :description
-  attr :link
+
+  def description
+    @database['description']
+  end
+
+  def link
+    @database['link']
+  end
+
+  def title
+    @database['title']
+  end
 end
