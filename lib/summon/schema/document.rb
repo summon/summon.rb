8f2d23ae938deab41555bfaf7a962cb215fc26c4
Mando Escamilla <mando@mando.org>
2010-05-04 11:40:43 -0500

    Added Publication Place support

--- lib/summon/schema/document.rb
+++ lib/summon/schema/document.rb
@@ -16,6 +16,7 @@ class Summon::Document < Summon::Schema
   attr :end_page
   attr :page_count
   attr :publication_date, :json_name => "PublicationDate_xml",  :transform => :Date
+  attr :publication_place
 
   attr :isi_cited_references_count, :json_name => "ISICitedReferencesCount"
   attr :isi_cited_references_uri, :json_name => "ISICitedReferencesURI"
