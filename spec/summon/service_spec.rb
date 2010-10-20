4b2398107c7c4f790e5dfb5b5d9ae691b57d436d
Daniël van de Burgt <daniel.vandeburgt@serialssolutions.com>
2010-10-20 13:42:02 +0200

    spec uncovering bug in cloning a service using []

--- spec/summon/service_spec.rb
+++ spec/summon/service_spec.rb
@@ -14,6 +14,17 @@ describe Summon::Service do
   it "has a default url which is the public production summon url" do
     Summon::Service.new.url.should == "http://api.summon.serialssolutions.com"
   end
+
+  it "allows cloning a service with overridden settings" do
+    options = {:url => "http://google.com", :access_id => "Bob", :secret_key => "Happiness", :session_id => "foo", :client_key => "Justice"}
+    service = Summon::Service.new(options)
+    another = service[{:access_id => "John"}]
+    another.access_id.should == "John"
+    another.client_key.should == options[:client_key]
+    another.url.should == options[:url]
+    another.instance_variable_get(:@secret_key).should == options[:secret_key]
+  end
+
   
   describe "Locale" do
   
